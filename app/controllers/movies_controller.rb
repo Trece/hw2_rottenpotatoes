f05155179a73e513568510837331f78289e4d934
Trece <treathen@hotmail.com>
2013-10-19 17:26:22 +0800

    Problem 3 that contains redirect.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,21 +7,40 @@ class MoviesController < ApplicationController
   end
 
   def index
+    missing_messege = false
     @all_ratings = Movie.all_ratings
     @range = @all_ratings 
-    ratings = if params['rating'] then params['rating'] else session['rating'] end
-    @flag = if params['sort_by'] then params['sort_by'] else session['sort_by'] end
-    if ratings then
-      @range = @all_ratings.select {|rating| rating if ratings[rating]}
+    ratings = if params['rating'] then 
+                params['rating'] 
+              else
+                if session['rating'] != params['rating']
+                  missing_messege = true
+                end
+                session['rating'] 
+              end
+    @flag = if params['sort_by'] then 
+              params['sort_by'] 
+            else
+              if session['sort_by'] != params['sort_by']
+                missing_messege = true
+              end
+              session['sort_by'] 
+            end
+    if missing_messege then
+      redirect_to movies_path({:sort_by => @flag, :rating => ratings})
+    else
+      if ratings then
+        @range = @all_ratings.select {|rating| rating if ratings[rating]}
+      end
+      @movies = Movie.where :rating => @range
+      if @flag == 'title' then
+        @movies = @movies.sort_by {|movie| movie.title}
+      elsif @flag == 'date' then
+        @movies = @movies.sort_by {|movie| movie.release_date} 
+      end
+      session['sort_by'] = @flag
+      session['rating'] = ratings
     end
-    @movies = Movie.where :rating => @range
-    if @flag == 'title' then
-      @movies = @movies.sort_by {|movie| movie.title}
-    elsif @flag == 'date' then
-      @movies = @movies.sort_by {|movie| movie.release_date} 
-    end
-    session['sort_by'] = @flag
-    session['rating'] = ratings
   end
 
   def new
